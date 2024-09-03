a88c5288cfd6afd57e4e82c313d962bd2f6a865b
Igor Contri <igorcontri@gmail.com>
2024-09-03 13:34:29 -0300

    exercicio_03

--- exercicio_03.py
+++ exercicio_03.py
@@ -7,6 +7,42 @@
 IMPORTANTE:
 a) Usar o json ou xml disponível como fonte dos dados do faturamento mensal;
 b) Podem existir dias sem faturamento, como nos finais de semana e feriados. Estes dias devem ser ignorados no cálculo da média;
+
 """
-vetor = []
+
+#  ============================= IMPORTANTE ============================= 
+
+# ESSE EXERCÍCIO DÁ A ENTENDER QUE HÁ UM ARQUIVO JSON/XML DISPONÍVEL COM DADOS DE FATURAMENTO
+# PORÉM NÃO ENCONTREI NENHUM ARQUIVO DISPONÍVEL NO SITE EM QUE REALIZEI O TESTE (GUPY)
+
+# DE FORMA A PRIORIZAR A LÓGICA, CRIEI MANUALMENTE UM ARQUIVO JSON, SIMULANDO O FATURAMENTO DIÁRIO DA EMPRESA
+# CONSIDERANDO FINAIS DE SEMANA E POSSÍVEIS FERIADOS (Que o exercício indica para serem ignorados).
+
+# ======================================================================= 
+
+import json
+
+def calcular_faturamento(json_data):
+    faturamento = json_data["faturamento_diario"]
+
+    faturamento_validos = [dia["valor"] for dia in faturamento if dia["valor"] > 0]
+
+    menor_faturamento = min(faturamento_validos)
+    maior_faturamento = max(faturamento_validos)
+
+    media_mensal = sum(faturamento_validos) / len(faturamento_validos)
+
+    dias_acima_media = sum(1 for valor in faturamento_validos if valor > media_mensal)
+    
+    return menor_faturamento, maior_faturamento, dias_acima_media
+
+with open("exercicio_03.json") as file:
+    json_data = json.load(file)
+
+menor, maior, dias_acima_media = calcular_faturamento(json_data)
+
+print(f"Menor Faturamento: R${menor:.2f}")
+print(f"Menor Faturamento: R${maior:.2f}")
+print(f"Dias de faturamento acima da média mensal: {dias_acima_media}")
+
 
